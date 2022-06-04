f3bcb1b68d50c7577e09a3594d2b8d1007009677
Phil Cohen <phillip@phillip.io>
2022-06-04 11:20:05 -0700

    all: initial commit

---
+++ src/extension.ts
@@ -0,0 +1,91 @@
+import * as vscode from "vscode";
+import { commands, Uri } from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  // vscode.window.showInformationMessage("Sidecar Loaded!");
+
+  function applyJetBrainsState() {
+    const fs = require("fs");
+    const os = require("os");
+
+    // TODO(pcohen): diff the state against the previous state
+    let state = JSON.parse(
+      fs.readFileSync(os.homedir() + "/.jb-state/latest.json")
+    );
+    let activeEditorState = state["activeEditor"];
+
+    let editor = vscode.window.activeTextEditor;
+
+    if (activeEditorState["path"] !== editor?.document.uri.path) {
+      // vscode.window.showInformationMessage("Changing paths to " + state["currentPath"]);
+
+      // TODO(pcohen): we need to make this blocking; I believe the commands below
+      // run too early when the currently opened file is changed.
+      commands.executeCommand(
+        "vscode.open",
+        Uri.file(state["activeEditor"]["path"])
+      );
+    }
+
+    commands.executeCommand("revealLine", {
+      lineNumber: activeEditorState["firstVisibleLine"] - 1,
+      at: "top",
+    });
+
+    if (editor) {
+      editor.selections = activeEditorState["cursors"].map(
+        (cursor: any) =>
+          new vscode.Selection(
+            cursor.line,
+            cursor.column,
+            cursor.line,
+            cursor.column
+          )
+      );
+    }
+
+    // Close the other tabs that might have been opened.
+    commands.executeCommand("workbench.action.closeOtherEditors");
+  }
+
+  const watcher = vscode.workspace.createFileSystemWatcher(
+    new vscode.RelativePattern(require("os").homedir() + "/.jb-state/", "**/*")
+  );
+
+  watcher.onDidChange((uri) => {
+    applyJetBrainsState();
+  });
+
+  watcher.onDidCreate((uri) => {
+    applyJetBrainsState();
+  });
+
+  // ================================================================================
+  // Extra commands (for debugging purposes)
+  // ================================================================================
+
+  //
+  // Opening file by path
+  //
+  context.subscriptions.push(
+    vscode.commands.registerCommand("sidecar.openPath", (path) => {
+      commands.executeCommand("vscode.open", Uri.file(path));
+    })
+  );
+
+  //
+  // Setting the cursor position(s)
+  //
+  context.subscriptions.push(
+    vscode.commands.registerCommand("sidecar.setCursor", (x, y, z, a) => {
+      let editor = vscode.window.activeTextEditor;
+      if (editor) {
+        // TODO(pcohen): multiple selections
+        editor.selections = [new vscode.Selection(x, y, z, a)];
+      }
+    })
+  );
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
