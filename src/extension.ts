5adb7ea879566333e3ed89b72581f57e6c0255b9
Phil Cohen <phillip@phillip.io>
2022-10-11 09:12:49 -0700

    extension: register file watchers before trying to set up the command server
    Makes it easier to work with multiple instances, since otherwise the file watchers won't be set up.

--- src/extension.ts
+++ src/extension.ts
@@ -7,8 +7,9 @@ export async function activate(context: vscode.ExtensionContext) {
   // NOTE(pcohen): can be used to debug code reloading issues
   // vscode.window.showInformationMessage("Cursorless sidecar started (v10)!");
 
-  startCommandServer();
   registerFileWatchers();
+  // NOTE(pcohen): this won't behave well if another instance of Code is already serving the socket
+  startCommandServer();
 }
 
 // this method is called when your extension is deactivated
