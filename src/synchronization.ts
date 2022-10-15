90e00ac64684508406308dc6bb4da308185ac326
Phil Cohen <phillip@phillip.io>
2022-10-15 09:48:44 -0700

    synchronization: cleanup

--- src/synchronization.ts
+++ src/synchronization.ts
@@ -1,14 +1,98 @@
 import * as vscode from "vscode";
 import { commands, Uri } from "vscode";
 
-import {
-  FEATURE_FLAG_ENABLED,
-  FEATURE_FLAG_PERFORM_SCROLLING,
-  readFlagFile,
-} from "./featureFlags";
+import { FEATURE_FLAG_ENABLED, readFlagFile } from "./featureFlags";
+
 import * as fs from "fs";
 import { CURSORLESS_ROOT_DIRECTORY } from "./constants";
 import * as path from "path";
+import { keys, map, property, zipObject } from "lodash";
+
+const columnFocusCommands = {
+  [vscode.ViewColumn.One]: "workbench.action.focusFirstEditorGroup",
+  [vscode.ViewColumn.Two]: "workbench.action.focusSecondEditorGroup",
+  [vscode.ViewColumn.Three]: "workbench.action.focusThirdEditorGroup",
+  [vscode.ViewColumn.Four]: "workbench.action.focusFourthEditorGroup",
+  [vscode.ViewColumn.Five]: "workbench.action.focusFifthEditorGroup",
+  [vscode.ViewColumn.Six]: "workbench.action.focusSixthEditorGroup",
+  [vscode.ViewColumn.Seven]: "workbench.action.focusSeventhEditorGroup",
+  [vscode.ViewColumn.Eight]: "workbench.action.focusEighthEditorGroup",
+  [vscode.ViewColumn.Nine]: "workbench.action.focusNinthEditorGroup",
+  [vscode.ViewColumn.Active]: "",
+  [vscode.ViewColumn.Beside]: "",
+};
+
+export async function focusEditor(editor: vscode.TextEditor) {
+  const viewColumn = getViewColumn(editor);
+  if (viewColumn != null) {
+    await commands.executeCommand(columnFocusCommands[viewColumn]);
+  }
+}
+
+function getViewColumn(
+  editor: vscode.TextEditor,
+): vscode.ViewColumn | undefined {
+  if (editor.viewColumn != null) {
+    return editor.viewColumn;
+  }
+}
+
+async function applyEditorStateToVscodeEditor(
+  editorState: any,
+  editor: vscode.TextEditor,
+) {
+  // If we got into a state where the editor has local changes, always revert them. Otherwise all subsequent
+  // commands will fail.
+  //
+  // Note that this shouldn't happen ideally. This can happen if chaining is attempted (need to find
+  // a better synchronization solution).
+  if (editor?.document.isDirty) {
+    await commands.executeCommand("workbench.action.files.revert");
+  }
+
+  let destPath = editorState["path"];
+
+  // TODO(pcohen): forward the language mode from the source editor, rather than just relying on the file extension
+  // (see workbench.action.editor.changeLanguageMode, but also, there is a direct
+  // API for this: vscode.languages.setLanguageId, and a voice command: "change language Python")
+
+  // Prefer the temporary file if it's available
+  if (editorState["temporaryFilePath"]) {
+    destPath = editorState["temporaryFilePath"];
+  }
+
+  if (destPath !== editor?.document.uri.path) {
+    // vscode.window.showInformationMessage("Changing paths to " + state["currentPath"]);
+
+    // TODO(pcohen): we need to make this blocking; I believe the commands below
+    // run too early when the currently opened file is changed.
+    await commands.executeCommand("vscode.open", Uri.file(destPath));
+  }
+
+  if (editor) {
+    if (editorState["selections"]) {
+      editor.selections = editorState["selections"].map((selection: any) => {
+        return new vscode.Selection(
+          selection.anchor.line,
+          selection.anchor.column,
+          selection.active.line,
+          selection.active.column,
+        );
+      });
+    } else {
+      // TODO(rntz): migrate to |editorState["selections"]|
+      editor.selections = editorState["cursors"].map(
+        (cursor: any) =>
+          new vscode.Selection(
+            cursor.line,
+            cursor.column,
+            cursor.line,
+            cursor.column,
+          ),
+      );
+    }
+  }
+}
 
 // ================================================================================
 // Applying the the primary/other editor's state
@@ -33,73 +117,60 @@ export async function applyPrimaryEditorState() {
       "utf8",
     ),
   );
-  let activeEditorState = state["activeEditor"];
 
-  let editor = vscode.window.activeTextEditor;
+  // map tempfilepath to vscode editor (eventually id to vscode editor)
+  const editorMap = zipObject(
+    map(vscode.window.visibleTextEditors, property("document.fileName")),
+    vscode.window.visibleTextEditors,
+  );
 
-  // If we got into a state where the editor has local changes, always revert them. Otherwise all subsequent
-  // commands will fail.
-  //
-  // Note that this shouldn't happen ideally. This can happen if chaining is attempted (need to find
-  // a better synchronization solution).
-  if (editor?.document.isDirty) {
-    await commands.executeCommand("workbench.action.files.revert");
-  }
+  let differentVisibleWindows = false;
+  let superiorEditorVisibleFileNames = map(
+    state["editors"],
+    property("temporaryFilePath"),
+  ) as string[];
 
-  let destPath = activeEditorState["path"];
+  superiorEditorVisibleFileNames.forEach((superiorEditorFileName: string) => {
+    if (keys(editorMap).indexOf(superiorEditorFileName) === -1) {
+      differentVisibleWindows = true;
+    }
+  });
 
-  // TODO(pcohen): forward the language mode from the source editor, rather than just relying on the file extension
-  // (see workbench.action.editor.changeLanguageMode, but also, there is a direct
-  // API for this: vscode.languages.setLanguageId, and a voice command: "change language Python")
+  let activeEditor;
 
-  // Prefer the temporary file if it's available
-  if (activeEditorState["temporaryFilePath"]) {
-    destPath = activeEditorState["temporaryFilePath"];
-  }
-
-  if (destPath !== editor?.document.uri.path) {
-    // vscode.window.showInformationMessage("Changing paths to " + state["currentPath"]);
-
-    // TODO(pcohen): we need to make this blocking; I believe the commands below
-    // run too early when the currently opened file is changed.
-    await commands.executeCommand("vscode.open", Uri.file(destPath));
-
+  if (differentVisibleWindows) {
     // Close the other tabs that might have been opened.
     // TODO(pcohen): this seems to always leave one additional tab open.
-    await commands.executeCommand("workbench.action.closeOtherEditors");
-  }
+    await commands.executeCommand("workbench.action.closeAllEditors");
 
-  if (readFlagFile(FEATURE_FLAG_PERFORM_SCROLLING, true)) {
-    commands.executeCommand("revealLine", {
-      lineNumber: activeEditorState["firstVisibleLine"] - 1,
-      at: "top",
+    state["editors"].forEach(async (editorState: any) => {
+      let vscodeEditor = await vscode.window.showTextDocument(
+        Uri.file(editorState["temporaryFilePath"]),
+        {
+          viewColumn: vscode.ViewColumn.Beside,
+        },
+      );
+
+      if (editorState["active"] === true) {
+        activeEditor = vscodeEditor;
+      }
+
+      await applyEditorStateToVscodeEditor(editorState, vscodeEditor);
+    });
+  } else {
+    state["editors"].forEach(async (editorState: any) => {
+      if (editorState["active"] === true) {
+        activeEditor = editorMap[editorState["temporaryFilePath"]];
+      }
+      await applyEditorStateToVscodeEditor(
+        editorState,
+        editorMap[editorState["temporaryFilePath"]],
+      );
     });
   }
 
-  if (editor) {
-    if (activeEditorState["selections"]) {
-      editor.selections = activeEditorState["selections"].map(
-        (selection: any) => {
-          return new vscode.Selection(
-            selection.anchor.line,
-            selection.anchor.column,
-            selection.active.line,
-            selection.active.column,
-          );
-        },
-      );
-    } else {
-      // TODO(rntz): migrate to |activeEditorState["selections"]|
-      editor.selections = activeEditorState["cursors"].map(
-        (cursor: any) =>
-          new vscode.Selection(
-            cursor.line,
-            cursor.column,
-            cursor.line,
-            cursor.column,
-          ),
-      );
-    }
+  if (activeEditor) {
+    await focusEditor(activeEditor);
   }
 }
 
